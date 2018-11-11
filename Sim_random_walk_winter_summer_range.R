4e52ca2f2145470470d9503e21d518edecfff626
Diego Ellis Soto <diego.ellis91@hotmail.com>
2018-11-10 19:55:30 -0500

    Update Sim_random_walk_winter_summer_range.R
    Simulation_works

--- Sim_random_walk_winter_summer_range.R
+++ Sim_random_walk_winter_summer_range.R
@@ -106,6 +106,7 @@ proj4string(last_point) = proj
   xysp %>% subset(Poop_event == 1) %>%
      points(col = 'brown', pch = 24, bg = alpha('chocolate4', 0.4 ))
   # points(tail(xysp@coords,1), col = 'blue', pch = 16)
+  return(steps.df)
 }
 
 rw_within_homerange(150, 200, lowland_UD_shp, 8)
@@ -113,5 +114,37 @@ rw_within_homerange(150, 200, lowland_UD_shp, 8)
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # Psidium guajava: Mean fruit weight: 59.8g and mean seeds per fruit 226. 
 # 10 seeds planted per treatment -> 80 % germinate -> 1 out of 1 makes it to shrub.
-
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 # Make an animation
+# --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
+library("animation")
+library("tweenr") # applies to both base R and ggplot2
+library("gganimate") # makes 
+library("dplyr")
+library("gapminder") # where our data comes from
+# Inputs:
+bbox(xysp)
+steps.df
+
+make_plot <- function(ndays ) { # function input takes number from 2 to 8
+  
+  step_A <- steps.df[((1:ndays) -1),] # If I put 10, show me the first 9 points here
+  step_B<- steps.df[((ndays) -1),] # The last point that will be input. zB if i input ndays 10 its the 10th day
+  plot(x = step_A$Longitude,y = step_A$Latitude, type = "l",col = alpha(gm.us2a$color, .3),
+       xlim = c(783869.35 ,784078.93), ylim = c(-79837.42 ,-79537.43))
+  points(x = step_B$Longitude,y=step_B$Latitude,col = alpha(gm.us2b$color, 1),cex = 1.3,pch = 16)
+  
+  # If the moving point (tortoise) in the animation has poop_event = 1 make a purple color
+  if(step_B$Poop_event == 1){
+    points(step_B$Longitude, step_B$Latitude,col = 'brown', pch = 24, bg = alpha('chocolate4', 0.4 ))
+  }
+  
+  step_A %>% subset(Poop_event == 1) %>% dplyr::select(Longitude, Latitude) %>%
+  points(col = 'brown', pch = 24, bg = alpha('chocolate4', 0.4 ))
+  
+  Sys.sleep(.5)
+}
+ndays = 150
+for (idx in 2:ndays) {
+  make_plot(idx) # loop.  # in order to use animation package we need to do this
+} # loop through soemthing that acts as frame for each we need to draw a picture
